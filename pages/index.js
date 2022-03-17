6bec4ee4bfc7ad6744234cdb520b56cfc2847191
rca-kangu <raul.amaral@kangu.com.br>
2022-03-17 17:25:40 -0300

    testando cache

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 
 import axios from 'axios'
 
+export async function getServerSideProps() {
+  const res = await axios.get('https://www.kangu.com.br/')
+  return {
+     props: { site: res.data}, revalidate: 7200 
+  }
+}
 
 export default function Home(props) {
-  console.log({props})
   return (
       <div dangerouslySetInnerHTML={{ __html: props.site }} />
   )
   
 }
 
-export async function getServerSideProps(context) {
-  const res = await axios.get('https://www.kangu.com.br/')
-  return {
-     props: { site: res.data}
-  }
-}
+
